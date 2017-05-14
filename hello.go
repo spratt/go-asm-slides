008dc95bef37b97791e7295368c372ccf05876b6
Simon Pratt <simon@pr4tt.com>
2017-05-14 15:51:02 -0400

    Cleanup indentation in hello.go

--- hello.go
+++ hello.go
@@ -2,15 +2,15 @@ package main
 
 import (
   "fmt"
-	add2 "github.com/spratt/go_asm_talk/add2"
+  add2 "github.com/spratt/go_asm_talk/add2"
 )
 
 func Add2GO(x uint64) uint64 {
-	return x + 2
+  return x + 2
 }
 
 func main() {
-	var x uint64 = 3
+  var x uint64 = 3
   fmt.Printf("The result of %d + 2 is: %d\n", x, Add2GO(x))
   fmt.Printf("The result of %d + 2 is: %d\n", x, add2.Add2ASM(x))
 }

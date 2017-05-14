8345b860dc2c67449722a6ff22c31d3c4223681f
Simon Pratt <simon@pr4tt.com>
2017-05-14 15:49:21 -0400

    Cleanup assembly

--- add2/add2.s
+++ add2/add2.s
@@ -1,9 +1,15 @@
-// func Add2ASM(x int) int
+// func Add2ASM(x uint64) uint64
 //
-// R8 contains the input number
-//
-TEXT ·Add2ASM(SB), 4, $0
+//                 \/ refers to the stack space used
+TEXT ·Add2ASM(SB), $0
+        // Move the value from the top of the stack into R8
         MOVQ x+0(FP), R8
+        
+        // R8 = 2 + R8
         ADDQ $2, R8
+        
+        // Move the value from R8 to the return address
         MOVQ R8, ret+8(FP)
+        
+        // Return
         RET
